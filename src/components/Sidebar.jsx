c5c6cd5d74d6ea30498b9a1dc78590ed3fa35de5
Tata Vishnu Suhaas <vishnusuhaas.thaha@gmail.com>
2023-02-10 12:24:47 +0530

    sidebar-header implemented

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import ChatIcon from "@mui/icons-material/Chat";
+import { MoreVert } from "@mui/icons-material";
+import { Avatar, IconButton } from "@mui/material";
+
 import classes from "./Sidebar.module.css";
 
 const Sidebar = () => {
   return (
     <div className={classes.sidebar}>
       <div className={classes.sidebar__header}>
-        <div className={classes.sidebar__headerRight}></div>
+        <Avatar src="https://th.bing.com/th/id/OIP.TDCPXOeMZSb0RM0rTaw3AAHaEK?pid=ImgDet&rs=1" />
+        <div className={classes.sidebar__headerRight}>
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
       </div>
     </div>
   );
